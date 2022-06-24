c078b72103d2c9f48c02ca4cdc8478e648277fc9
Arling Holguin <arlingholguin@gmail.com>
2022-06-23 23:31:31 -0500

    Ajuste flex menú

---
+++ scritp.js
@@ -0,0 +1,6 @@
+// var year = 2022
+// var nombre = prompt('Nombre')
+// var edad = prompt('Edad')
+// var yearNacimiento = year - edad
+
+// alert(nombre + ' nació en el año ' + yearNacimiento + ' tiene ' + edad + ' años.')
